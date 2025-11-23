84e0b9d02c2324f0d3c09cb43de7146c7a4ad0df
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-23 12:06:26 -0300

    :style: Atualiza o estilo do arquivo source/MainViewController.h

--- source/MainViewController.h
+++ source/MainViewController.h
@@ -3,8 +3,9 @@
 #include <memory>
 #include <bdn/ui/Window.h>
 #include <bdn/ui/TextField.h>
-#include <bdn/ui/Label.h>
 #include <bdn/ui/Button.h>
+#include <bdn/ui/Label.h>
+#include <bdn/ui/ContainerView.h>
 
 class MainViewController
 {
@@ -13,10 +14,18 @@ class MainViewController
     virtual ~MainViewController() = default;
 
   private:
-    std::shared_ptr<bdn::ui::Window>    _window;
-    std::shared_ptr<bdn::ui::TextField> _aField;
-    std::shared_ptr<bdn::ui::TextField> _bField;
-    std::shared_ptr<bdn::ui::Label>     _resultLabel;
-    std::shared_ptr<bdn::ui::Button>    _sumButton;
+    std::shared_ptr<bdn::ui::Window>        _window;
+
+    std::shared_ptr<bdn::ui::ContainerView> _root;
+    std::shared_ptr<bdn::ui::ContainerView> _card;
+
+    std::shared_ptr<bdn::ui::Label>         _titleLabel;
+    std::shared_ptr<bdn::ui::Label>         _labelA;
+    std::shared_ptr<bdn::ui::Label>         _labelB;
+
+    std::shared_ptr<bdn::ui::TextField>     _aField;
+    std::shared_ptr<bdn::ui::TextField>     _bField;
+    std::shared_ptr<bdn::ui::Button>        _sumButton;
+    std::shared_ptr<bdn::ui::Label>         _resultLabel;
 };
 
