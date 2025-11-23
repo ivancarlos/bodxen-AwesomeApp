5345287d1b7f5fdfe7109694bf15e20cd15033cb
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-23 11:45:09 -0300

    :style: Atualiza o estilo do arquivo source/AppController.h

--- source/AppController.h
+++ source/AppController.h
@@ -1,14 +1,31 @@
 #pragma once
 
-#include "MainViewController.h"
-#include <bdn/ui/UIApplicationController.h>
+#include <memory>
+#include <bdn/ui/Window.h>
+#include <bdn/ui/TextField.h>
+#include <bdn/ui/Button.h>
+#include <bdn/ui/Label.h>
+#include <bdn/ui/ContainerView.h>
 
-class AppController : public bdn::ui::UIApplicationController
+class MainViewController
 {
   public:
-    void beginLaunch() override;
+    MainViewController();
+    virtual ~MainViewController() = default;
 
-  protected:
-    std::shared_ptr<MainViewController> _mainViewController;
+  private:
+    std::shared_ptr<bdn::ui::Window>        _window;
+
+    std::shared_ptr<bdn::ui::ContainerView> _root;
+    std::shared_ptr<bdn::ui::ContainerView> _card;
+
+    std::shared_ptr<bdn::ui::Label>         _titleLabel;
+    std::shared_ptr<bdn::ui::Label>         _labelA;
+    std::shared_ptr<bdn::ui::Label>         _labelB;
+
+    std::shared_ptr<bdn::ui::TextField>     _aField;
+    std::shared_ptr<bdn::ui::TextField>     _bField;
+    std::shared_ptr<bdn::ui::Button>        _sumButton;
+    std::shared_ptr<bdn::ui::Label>         _resultLabel;
 };
 
